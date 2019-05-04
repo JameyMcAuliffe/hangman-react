2fd080f8324d8d42e751a76f225b3f2ea52d95d9
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2019-05-04 18:05:34 -0500

    game and background components started, background image and gallows render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import Game from './Game/Game';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hangman</h1>
+      <Game />
     </div>
   );
 }
 
 export default App;
+
+
+// <img src={Gallows} alt="gallows" style={{"width": "40%", "backgroundColor": "beige"}}/>
+//<img src={Background} alt="background" style={{"height": "vh"}}/>
