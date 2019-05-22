25b930f60816a2e77c57e14c962c07679ba0dc56
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2019-05-21 20:53:50 -0500

    keypad disabled after 6 misses

--- src/Game/Figure/Figure.js
+++ src/Game/Figure/Figure.js
@@ -1,26 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 import classes from './Figure.module.css';
 import StickFigure from '../../images/figure.png';
 
 const Figure = (props) => {
-
-	//const [showRed, setShowRed] = useState(false);
-	//const [currentCount, setCurrentCount] = useState(0);
-
-	// useEffect(() => {
-	// 	console.log('mc: ', props.missCount);
-	// 	if (props.missCount > currentCount) {
-	// 		setTimeout(() => {
-	// 			setShowRed(true);
-	// 		}, 1000)
-	// 	}
-
-	// 	setShowRed(false);
-	// 	setCurrentCount(props.missCount);
-	// });
-
-	//let miss = showRed ? classes.MissedFigureRedDiv : classes.MissedFigureDiv;
 
 	return (
 		<div className={props.missCount >= props.number ? classes.MissedFigureDiv : classes.FigureDiv}>
